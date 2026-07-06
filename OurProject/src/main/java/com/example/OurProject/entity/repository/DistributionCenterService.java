d9583d2d9bf1d3bcf36dc43db58aaba7b600bcb5
AlexeyNN1581 <alesha_gorelikov52@mail.ru>
2026-07-06 13:27:56 +0300

    init commit

--- OurProject/src/main/java/com/example/OurProject/entity/repository/DistributionCenterService.java
+++ OurProject/src/main/java/com/example/OurProject/entity/repository/DistributionCenterService.java
@@ -2,10 +2,12 @@ package com.example.OurProject.entity.repository;
 
 import com.example.OurProject.entity.DistributionCenter;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class DistributionCenterService {
@@ -25,7 +27,7 @@ public class DistributionCenterService {
 
     public DistributionCenter getById(Long id) {
 
-        return baseConnectionRepository.findById(id).orElseThrow(() -> new RuntimeException("Сущность с ID " + id + " не найдена"));
+        return baseConnectionRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Сущность с ID " + id + " не найдена"));
     }
 @NotNull
     public DistributionCenter update(Long id, DistributionCenter distributionCenter) {
